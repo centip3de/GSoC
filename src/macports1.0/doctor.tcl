4e40a42730fcc87654cba94ae1e6342b8d17890c
centip3de <goodkingkyle@gmail.com>
2014-07-01 12:25:55 -0700

    Updated and simplified the parser, added parsing options for xcode-versions, added the ability to check for the wrong/right xcode version based on OSX version.

--- src/macports1.0/doctor.tcl
+++ src/macports1.0/doctor.tcl
@@ -1,6 +1,12 @@
 
+
+# Todo:
+# Move port_doctor.ini to the port tree, below _resources 
+# Command-Line tools version check
+
 package provide doctor 1.0 
 package require macports
+package require portutil 1.0
 
 namespace eval doctor {
     
@@ -15,8 +21,30 @@ namespace eval doctor {
 
         array set config_options [list]
 
-        get_config config_options
-        check_path $config_options("macports_location") $config_options("profile_path") $config_options("shell_loc")
+        set parser_options {"macports_location" "profile_path" "shell_location" "xcode_version_10.9" "xcode_version_10.8" \
+                            "xcode_version_10.7" "xcode_version_10.6" "xcode_version_10.7" "xcode_version_10.6" "xcode_version_10.5" \
+                            "xcode_version_10.4" "xcode_build"}
+
+        get_config config_options $parser_options
+        check_path $config_options(macports_location) $config_options(profile_path) $config_options(shell_location)
+        check_xcode config_options
+    }
+
+    proc check_xcode {config_options} {
+        upvar $config_options config 
+
+        set mac_version     ${macports::macosx_version}
+        set xcode_current   ${macports::xcodeversion} 
+        set xcode_versions  $config(xcode_version_$mac_version)
+
+        if {$xcode_current in $xcode_versions} {
+            return
+        
+        } else {
+            ui_error "currently installed version of Xcode, $xcode_current, is not supported by MacPorts. \
+                      For your currently installed system, only the following versions of Xcode are supported: \
+                      $xcode_versions"
+        }
     }
 
     proc make_default_config {} {
@@ -34,25 +62,31 @@ namespace eval doctor {
         puts $fd "macports_location=/opt/local"
         puts $fd "profile_path=${macports::user_home}/.bash_profile"
         puts $fd "shell_location=/bin/bash"
-        puts $fd "xcode_version=4.14"
+        puts $fd "xcode_version_10.9=5.1.1 5.1 5.0.2 5.0.1"
+        puts $fd "xcode_version_10.8=5.1 5.0.2 5.0.1 5.0 4.6.3 4.6.2 4.6.1 4.6 4.5.2 4.5.1 4.5"
+        puts $fd "xcode_version_10.7=4.6.3 4.6.2 4.6.1 4.6 4.5.2 4.5.1 4.5 4.3.3"
+        puts $fd "xcode_version_10.6=4.2 3.2.6 3.2.5 3.2.4 3.2.3 3.2.2 3.2.1 3.2"
+        puts $fd "xcode_version_10.5=3.1.4 3.1.3 3.1.2 3.1.1 3.1 3.0"
+        puts $fd "xcode_version_10.4=2.5 2.4.1 2.4 2.3 2.2.1 2.2 2.1 2.0"
+        puts $fd "xcode_build=5B1008"
         
         close $fd
    }
 
-    proc get_config {config_options} {
+    proc get_config {config_options parser_options} {
 
         # Reads in and parses the configureation file, port_doctor.ini. After parsing, all variables found are assigned 
         # in the 'config_options' associative array.
         #
         # Args:
         #           config_options - The associative array responsible for holding all the configuration options.
+        #           parser_options - The list responsible for holding each option to set/look for in the configuration file.
         # Returns:
         #           None. 
 
         upvar $config_options config 
 
-        set path        ${macports::portdbpath}/port_doctor.ini
-        set return_args [list]
+        set path ${macports::portdbpath}/port_doctor.ini
 
         if {[file exists $path] == 0} {
             ui_warn "No configuration file found. Creating generic config file."
@@ -68,20 +102,11 @@ namespace eval doctor {
         foreach line $data { 
             set tokens [split $line "="]
 
-            if {[lindex $tokens 0] eq "macports_location"} {
-                set config("macports_location") [lindex $tokens 1]
-
-            } elseif {[lindex $tokens 0] eq "profile_path"} {
-                set config("profile_path") [lindex $tokens 1]
-
-            } elseif {[lindex $tokens 0] eq "xcode_version"} {
-                set config("xcode_version") [lindex $tokens 1]
-
-            } elseif {[lindex $tokens 0] eq "shell_location"} {
-                set config("shell_loc") [lindex $tokens 1]
-
+            if {[lindex $tokens 0] in $parser_options} {
+                set config([lindex $tokens 0]) [lindex $tokens 1]
+            
             } elseif {[lindex $tokens 0] eq ""} {
-              continue
+                continue
 
             } else {
                 ui_error "unrecognized port_doctor.ini config option: [lindex $tokens 0]"
